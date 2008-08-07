0826f62e585e527037558779136c8900ea8fb081
mirkosertic <mirkosertic@d3b2640a-98bb-4239-95bf-c7bbe60608d9>
2008-08-07 18:21:05 +0000

    Added Login Screen again

---
+++ trunk/mogwai/MogwaiPowerstaff/src/main/java/de/powerstaff/business/service/impl/AuthenticationListenerImpl.java
@@ -0,0 +1,7 @@
+package de.powerstaff.business.service.impl;
+
+import de.mogwai.common.security.AuthenticationListenerAdapter;
+
+public class AuthenticationListenerImpl extends AuthenticationListenerAdapter {
+
+}
--- trunk/mogwai/MogwaiPowerstaff/src/main/java/de/powerstaff/web/backingbean/login/LoginBackingBean.java
+++ trunk/mogwai/MogwaiPowerstaff/src/main/java/de/powerstaff/web/backingbean/login/LoginBackingBean.java
@@ -1,40 +1,41 @@
 package de.powerstaff.web.backingbean.login;
 
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.ui.WebAuthenticationDetails;
+
+import de.mogwai.common.business.service.LoginException;
+import de.mogwai.common.business.service.LoginService;
 import de.mogwai.common.web.backingbean.WrappingBackingBean;
 import de.mogwai.common.web.utils.JSFMessageUtils;
-import de.powerstaff.business.service.AuthenticationService;
 import de.powerstaff.web.backingbean.MessageConstants;
 
 public class LoginBackingBean extends WrappingBackingBean<LoginBackingBeanDataModel> implements MessageConstants {
 
-    private AuthenticationService authenticationService;
-
-    /**
-     * @return the authenticationService
-     */
-    public AuthenticationService getAuthenticationService() {
-        return authenticationService;
-    }
-
-    /**
-     * @param authenticationService
-     *                the authenticationService to set
-     */
-    public void setAuthenticationService(AuthenticationService authenticationService) {
-        this.authenticationService = authenticationService;
-    }
+    private LoginService loginService;
 
     @Override
     protected LoginBackingBeanDataModel createDataModel() {
         return new LoginBackingBeanDataModel();
     }
+    
+    public LoginService getLoginService() {
+        return loginService;
+    }
+
+    public void setLoginService(LoginService loginService) {
+        this.loginService = loginService;
+    }
 
     public String commandLogin() {
-        if (authenticationService.login(getData().getUsername(), getData().getPassword()) != null) {
+        try {
+            HttpServletRequest theRequest = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+            loginService.login(getData().getUsername(), getData().getPassword(), new WebAuthenticationDetails(theRequest));
             return "INDEX";
+        } catch (LoginException e) {
+            JSFMessageUtils.addGlobalErrorMessage(MSG_FALSCHESLOGIN);
+            return null;
         }
-
-        JSFMessageUtils.addGlobalErrorMessage(MSG_FALSCHESLOGIN);
-        return null;
     }
 }
