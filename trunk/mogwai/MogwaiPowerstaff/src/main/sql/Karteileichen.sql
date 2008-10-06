617be5893543601f7e0a0926576d96760e4195d2
mirkosertic <mirkosertic@d3b2640a-98bb-4239-95bf-c7bbe60608d9>
2008-10-06 18:25:21 +0000

    Aktualisierungsscript für Karteileichen

---
+++ trunk/mogwai/MogwaiPowerstaff/src/main/sql/Karteileichen.sql
@@ -0,0 +1,36 @@
+select count(*) from freelancer_udf WHERE name = 'ansp' and stringValue is not null
+select count(*) from freelancer_udf WHERE name = 'anspID' and intvalue is not null
+
+select * from partner_udf where intvalue = -42062652
+select * from customer_udf where intvalue = -42062652
+
+select a.freelancer_id, CONCAT('\nVermittelt von ',a.stringValue) from freelancer_udf a, customer_udf b where a.name='ansp' and a.stringValue is not null
+
+select CONCAT('Vermittelt von ',b.stringValue, '\n', a.comments) ,a.* from 
+    freelancer a
+    inner join freelancer_udf b on (a.id = b.freelancer_id)
+where 
+    b.name = 'ansp' and
+    b.stringValue is not null 
+
+update freelancer a
+    inner join freelancer_udf b on (a.id = b.freelancer_id)
+set
+    a.comments =  concat('Vermittelt von ',b.stringValue, '\n', a.comments)
+where 
+    b.name = 'ansp' and
+    b.stringValue is not null
+
+select a.freelancer_id, CONCAT('\nVermittelt von ',c.name1,',', c.name2, '\n' ,c.company) from freelancer_udf a, customer_udf b , customer c where a.name='anspID' and a.intvalue is not null and a.intvalue <> 0 and b.name = 'ID' and a.intvalue = b.intvalue and b.customer_id = c.id
+
+update freelancer a
+    inner join freelancer_udf b on (a.id = b.freelancer_id)
+    inner join customer_udf c on (c.intvalue = b.intvalue)
+    inner join customer d on (c.customer_id = d.id)
+set
+    a.comments = concat('Vermittelt von ',d.name1,', ', d.name2, ', ' ,d.company,'\n',a.comments)
+where 
+    b.name = 'anspID' and
+    b.intvalue is not null and
+    b.intvalue <> 0 and 
+    c.name = 'ID'
