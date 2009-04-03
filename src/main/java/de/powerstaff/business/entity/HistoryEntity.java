3bc144526f274a8d382aba2e92a7a7b81327db0e
Mirko Sertic <mirko@mirkosertic.de>
2009-04-03 19:23:40 +0000

    Kategorisierung der Kontakthistorie im Domänenmodell aufgenommen

--- src/main/java/de/powerstaff/business/entity/HistoryEntity.java
+++ src/main/java/de/powerstaff/business/entity/HistoryEntity.java
@@ -6,11 +6,21 @@ public class HistoryEntity extends AuditableEntity {
 
     private String description;
 
+    private HistoryType type;
+
     public String getDescription() {
         return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public HistoryType getType() {
+        return type;
+    }
+
+    public void setType(HistoryType type) {
+        this.type = type;
     }
 }
---
+++ src/main/java/de/powerstaff/business/entity/HistoryType.java
@@ -0,0 +1,16 @@
+package de.powerstaff.business.entity;
+
+import de.mogwai.common.business.entity.Entity;
+
+public class HistoryType extends Entity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
