55f83dd8bb9a0d5a5a1ddcceaf5a7ac338e34d9a
Mirko Sertic <mirko@mirkosertic.de>
2010-01-07 11:05:25 +0000

    Migrated document content extraction to Apache POI

---
+++ src/main/java/de/powerstaff/business/service/impl/reader/AbstractDocumentReader.java
@@ -0,0 +1,39 @@
+/**
+ * Mogwai PowerStaff. Copyright (C) 2002 The Mogwai Project.
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package de.powerstaff.business.service.impl.reader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public abstract class AbstractDocumentReader implements DocumentReader {
+
+	protected String toFlatString(Reader aReader) throws IOException {
+        StringBuilder theBuilder = new StringBuilder();
+        BufferedReader theReader = new BufferedReader(aReader);
+        while (theReader.ready()) {
+            String theLine = theReader.readLine();
+            if (theLine != null) {
+                theBuilder.append(theLine);
+                theBuilder.append(" ");
+            }
+        }
+        theReader.close();
+        return theBuilder.toString();
+	}
+}
